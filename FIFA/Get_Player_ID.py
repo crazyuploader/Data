d4c81c066689449a4e94054e7deaae38e4b8ccdb
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-03-28 13:51:12 +0530

    Update:
    FIFA/Get_Player_ID.py
    FIFA/Get_Player_Info.py
    
    Code clean up

--- FIFA/Get_Player_ID.py
+++ FIFA/Get_Player_ID.py
@@ -10,17 +10,17 @@ import pandas
 # Empty List
 Player_ID = []
 
-request = requests.get("https://www.fifa.com/worldcup/players/_libraries/byposition/[id]/_players-list")
+request = requests.get(
+    "https://www.fifa.com/worldcup/players/_libraries/byposition/[id]/_players-list"
+)
 soup = BeautifulSoup(request.content, "html.parser")
 
 # 'for' iteration to find all IDs
 for IDs in range(0, 736):
     all = soup.find_all("a", "fi-p--link")[IDs]
-    Player_ID.append(all['data-player-id'])
+    Player_ID.append(all["data-player-id"])
 
 # Data frame to store scrapped data
-df = pandas.DataFrame({
-    "IDs": Player_ID
-})
-df.to_csv('Player_IDs.csv', index=False)
+df = pandas.DataFrame({"IDs": Player_ID})
+df.to_csv("Player_IDs.csv", index=False)
 print(df)
--- FIFA/Get_Player_Info.py
+++ FIFA/Get_Player_Info.py
@@ -21,8 +21,8 @@ for pages in Ids:
     # Using OrderedDict instead of Dict (See explaination)
     d = OrderedDict()
     # Fetching URLs one by one
-    print(base_url+str(pages)+"/profile.html")
-    request = requests.get(base_url+str(pages)+"/profile.html")
+    print(base_url + str(pages) + "/profile.html")
+    request = requests.get(base_url + str(pages) + "/profile.html")
     # Data Processing
     content = request.content
     soup = BeautifulSoup(content, "html.parser")
@@ -30,20 +30,40 @@ for pages in Ids:
     # Name # Country # Role # Age # Height # International Caps # International Goals
     # Creating an exception handler just in case the page doesn't have any Data
     try:
-        d['Name'] = soup.find("div", {"class": "fi-p__name"}).text.replace("\n", "").strip()
+        d["Name"] = (
+            soup.find("div", {"class": "fi-p__name"}).text.replace("\n", "").strip()
+        )
     except:
         print("No Data Available for Player")
         continue
-    print(d['Name'])
-    d['Country'] = soup.find("div", {"class": "fi-p__country"}).text.replace("\n", "").strip()
-    d['Role'] = soup.find("div", {"class": "fi-p__role"}).text.replace("\n", "").strip()
-    d['Age'] = soup.find("div", {"class": "fi-p__profile-number__number"}).text.replace("\n", "").strip()
-    d['Height(cm)'] = soup.find_all("div", {"class": "fi-p__profile-number__number"})[1].text.replace("\n", "").strip()
-    d['International Caps'] = soup.find_all("div", {"class": "fi-p__profile-number__number"})[2].text.replace("\n", "").strip()
-    d['International Goals'] = soup.find_all("div", {"class": "fi-p__profile-number__number"})[3].text.replace("\n", "").strip()
+    print(d["Name"])
+    d["Country"] = (
+        soup.find("div", {"class": "fi-p__country"}).text.replace("\n", "").strip()
+    )
+    d["Role"] = soup.find("div", {"class": "fi-p__role"}).text.replace("\n", "").strip()
+    d["Age"] = (
+        soup.find("div", {"class": "fi-p__profile-number__number"})
+        .text.replace("\n", "")
+        .strip()
+    )
+    d["Height(cm)"] = (
+        soup.find_all("div", {"class": "fi-p__profile-number__number"})[1]
+        .text.replace("\n", "")
+        .strip()
+    )
+    d["International Caps"] = (
+        soup.find_all("div", {"class": "fi-p__profile-number__number"})[2]
+        .text.replace("\n", "")
+        .strip()
+    )
+    d["International Goals"] = (
+        soup.find_all("div", {"class": "fi-p__profile-number__number"})[3]
+        .text.replace("\n", "")
+        .strip()
+    )
     player_list.append(d)
     # Create a pandas DataFrame to store data and save it to .csv
     df = pandas.DataFrame(player_list)
-    df.to_csv('Players_Info.csv', index=False)
+    df.to_csv("Players_Info.csv", index=False)
 
 print("\nSuccess \n")
